b804e09b4b015a066c652781c4b1cf73b632d727
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-07-12 09:30:28 +0200

    refactor: implemented authentication against MyAnimeList oauth2 server

--- src/authentication.ts
+++ src/authentication.ts
@@ -13,11 +13,35 @@ export function getAuthenticationConfig(): AuthConfig {
     const authenticationConfig: AuthConfig = {
         response_type: "code",
         client_id: "b99c2ab2c2fd5e6a0d0cb7ea666892ef",
-        redirect_uri: [], // todo add redirect_uri (also to MyAnimeList API settings)
+        redirect_uri: ["https://minatore0712.github.io/AniWatch/"],
         code_challenge: generateCodeChallenge(generateCodeVerifier()),
         code_challenge_method: "plain",
     };
     return authenticationConfig;
+}
+
+export async function authenticate(config: AuthConfig): Promise<unknown> {
+    let url = `https://myanimelist.net/v1/oauth2/authorize?`;
+    url = url.concat(`response_type=${config.response_type}`);
+    url = url.concat(`&client_id${config.client_id}`);
+    url = url.concat(`&code_challenge${config.code_challenge}`);
+
+    if (config.state) {
+        url = url.concat(`&state${config.state}`);
+    }
+
+    if (config.redirect_uri && config.redirect_uri.length > 0) {
+        url = url.concat(`&redirect_uri${config.redirect_uri}`);
+    }
+
+    if (config.code_challenge_method) {
+        url = url.concat(
+            `&code_challenge_method${config.code_challenge_method}`
+        );
+    }
+
+    const response = await fetch(url);
+    return response;
 }
 
 /**
